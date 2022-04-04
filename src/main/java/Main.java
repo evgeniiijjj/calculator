12d96dc78f141a8ff7f738d100f2d85f3707180d
evgeniiijjj <100117060+evgeniiijjj@users.noreply.github.com>
2022-04-04 14:12:16 +0300

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,7 +7,7 @@ public class Main {
         int a = calc.plus.apply(1, 2);
         int b = calc.minus.apply(1, 1);// b равно 0;
 
-        if (calc.isPositive.test(b)) calc.println.accept(calc.devide.apply(a, b).toString());// перед делением необходима проверка делителя на равенство нулю;
-        else calc.println.accept("делитель равен нулю");
+        if (calc.is0.test(b)) calc.println.accept("делитель равен нулю");// перед делением необходима проверка делителя на равенство нулю;
+        else calc.println.accept(calc.devide.apply(a, b).toString());
     }
 }
