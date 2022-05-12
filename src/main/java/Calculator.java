8c4bf8f59f950d7fcf0ff823cb026d1c0e0d7946
evgeniiijjj <evgenijjj78@gmail.com>
2022-05-12 12:27:35 +0300

    commit unit tests

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -7,12 +7,16 @@ public class Calculator {
     public BinaryOperator<Integer> plus = (x, y) -> x + y;
     public BinaryOperator<Integer> minus = (x, y) -> x - y;
     public BinaryOperator<Integer> multiply = (x, y) -> x * y;
-    public BinaryOperator<Integer> devide = (x, y) -> x / y;
+    public BinaryOperator<Integer> divide = (x, y) -> {
+        if (y == 0) throw new ArithmeticException("делитель равен нулю");
+        return x / y;
+    };
 
     public UnaryOperator<Integer> pow = x -> x * x;
     public UnaryOperator<Integer> abs = x -> x > 0 ? x : -x;
 
     public Predicate<Integer> isPositive = x -> x > 0;
+    public Predicate<Integer> is0 = x -> x == 0;
 
     public Consumer<String> println = System.out::println;
 }
--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,2 +1,75 @@
-package PACKAGE_NAME;public class MainTest {
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MainTest {
+    static Calculator calc = initCalculator();
+
+    static Calculator initCalculator() {
+        return Calculator.instance.get();
+    }
+
+    @Test
+    public void successCreateCalculator() {
+        Assertions.assertNotNull(calc);
+    }
+
+    @Test
+    public void checkPlusCalc() {
+        int arg1 = 5;
+        int arg2 = 3;
+        int expectedResult = 8;
+        int result = calc.plus.apply(arg1, arg2);
+        Assertions.assertEquals(expectedResult, result);
+    }
+
+    @Test
+    public void checkMinusCalc() {
+        int arg1 = 5;
+        int arg2 = 3;
+        int expectedResult = 2;
+        int result = calc.minus.apply(arg1, arg2);
+        Assertions.assertEquals(expectedResult, result);
+    }
+
+    @Test
+    public void checkMultiplyCalc() {
+        int arg1 = 5;
+        int arg2 = 3;
+        int expectedResult = 15;
+        int result = calc.multiply.apply(arg1, arg2);
+        Assertions.assertEquals(expectedResult, result);
+    }
+
+    @Test
+    public void checkDivideCalc() {
+        int arg1 = 21;
+        int arg2 = 3;
+        int expectedResult = 7;
+        int result = calc.divide.apply(arg1, arg2);
+        Assertions.assertEquals(expectedResult, result);
+    }
+
+    @Test
+    public void checkPowCalc() {
+        int arg = 5;
+        int expectedResult = 25;
+        int result = calc.pow.apply(arg);
+        Assertions.assertEquals(expectedResult, result);
+    }
+
+    @Test
+    public void checkAbsCalc() {
+        int arg = -17;
+        int expectedResult = 17;
+        int result = calc.abs.apply(arg);
+        Assertions.assertEquals(expectedResult, result);
+    }
+
+    @Test
+    public void checkDivideException() {
+        int arg1 = 5;
+        int arg2 = 0;
+        Assertions.assertThrows(ArithmeticException.class, () -> calc.divide.apply(arg1, arg2));
+    }
+
 }
